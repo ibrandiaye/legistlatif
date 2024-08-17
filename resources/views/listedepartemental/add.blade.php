b7fc33e9f715ef59e825d8342962d9bd9c96b2b9
unknown <ibra789ndiaye@gmail.com>
2024-08-17 19:59:47 +0000

    leg  15

--- resources/views/listedepartemental/add.blade.php
+++ resources/views/listedepartemental/add.blade.php
@@ -210,7 +210,7 @@
                 $.blockUI({ message: "<p>Patienter</p>" }); 
                 $.ajax({
             type:'GET',
-           /url:'http://127.0.0.1:7777/api/cartes/get/by/nin?nin='+cni,
+           url:'http://127.0.0.1:7777/api/cartes/get/by/nin?nin='+cni,
          //  url: 'http://5.189.166.92:7777/api/cartes/get/by/nin?nin='+cni,
           
             data:'_token = <?php echo csrf_token() ?>',
