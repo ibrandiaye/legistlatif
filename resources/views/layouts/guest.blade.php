4e6d26b8eb8bf0b3fdd8fc9adedbb1bec0c5d4a4
unknown <ibra789ndiaye@gmail.com>
2024-09-14 23:32:24 +0000

    second  commit 12

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,10 +15,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
+    
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <img src="{{ asset('images/logo_n-removebg-preview.png') }}" style="height: 150px;">
                 </a>
             </div>
 
